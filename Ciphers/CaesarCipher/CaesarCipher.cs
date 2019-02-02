13fbc5fb583301f44bc3e7d905a95601bcd85ad5
Murad <murad.ragimow2902@gmail.com>
2019-02-02 12:40:40 +0400

    Caesar cipher added

---
+++ Ciphers/CaesarCipher/CaesarCipher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ciphers.CaesarCipher
+{
+    class CaesarCipher
+    {
+        public string Encription(string text)
+        {
+            string cipherText = "";
+            int n = 3;
+            int x = 0;
+
+            foreach (var item in text)
+            {
+                if (item != 32 && item != 160)
+                {
+                    x = item - n;
+                    cipherText += Convert.ToChar(x);
+                }
+                else
+                    cipherText += " ";
+            }
+
+            return cipherText;
+        }
+    }
+}
