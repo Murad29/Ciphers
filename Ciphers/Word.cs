429714017d4145a85cd4792ddf3846d5de7bb118
Murad <murad.ragimow2902@gmail.com>
2019-02-02 12:34:42 +0400

    Initial Commit

---
+++ Ciphers/Word.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ciphers
+{
+    class Word
+    {
+        public string Text { get; set; }
+        public string CipherText { get; set; }
+    }
+}
